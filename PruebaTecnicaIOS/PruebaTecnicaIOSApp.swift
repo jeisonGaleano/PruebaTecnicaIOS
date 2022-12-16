24105f3cc5ed35e9b1ce1dbd5a0d936e54601081
Jeison Galeano <jgaleano.extreme@gmail.com>
2022-12-16 09:59:32 -0500

    Initial Commit

---
+++ PruebaTecnicaIOS/PruebaTecnicaIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PruebaTecnicaIOSApp.swift
+//  PruebaTecnicaIOS
+//
+//  Created by Jeison Galeano on 16/12/22.
+//
+
+import SwiftUI
+
+@main
+struct PruebaTecnicaIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
